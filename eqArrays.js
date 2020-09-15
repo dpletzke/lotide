5e034414b325d3bd8c5815ad200e3a8a3d065459
Dan Pletzke <dpletzke@gmail.com>
2020-09-15 20:27:42 +0000

    initial solution

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let output = actual === expected ? `✅ Assertion Passed: [${actual}] === [${expected}]` : `🛑 Assertion Failed: [${actual}] !== [${expected}]`;
+  console.log(output);
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+} 
+
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
