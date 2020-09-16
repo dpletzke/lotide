8488cb2dd431aeb7b047ad8d1a9b4cda152ebe79
Dan Pletzke <dpletzke@gmail.com>
2020-09-16 18:04:41 +0000

    initial solution

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  let output = actual === expected ? `✅ Assertion Passed: [${actual}] === [${expected}]` : `🛑 Assertion Failed: [${actual}] !== [${expected}]`;
+  console.log(output);
+};
+
+
+//count when, keys with truthy value, 
+// don't count when, set to false, or not in object, or not in string
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if(itemsToCount[item]){
+      if(!results[item]) {
+        results[item] = 1;
+      }
+      else {
+        results[item]++;
+      }
+    }
+  }
+  console.log(results);
+  return results;
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
