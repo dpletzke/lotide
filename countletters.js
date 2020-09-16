b75402816420f151e19d1863736c40a29403b5d1
Dan Pletzke <dpletzke@gmail.com>
2020-09-16 18:16:26 +0000

    initial solution

---
+++ countletters.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+// const assertEqual = function(actual, expected) {
+//   let output = actual === expected ? `✅ Assertion Passed: [${actual}] === [${expected}]` : `🛑 Assertion Failed: [${actual}] !== [${expected}]`;
+//   console.log(output);
+// };
+
+let countLetters = function(str) {
+  const results = {};
+  for (const c of str) {
+    if (results[c]) {
+      results[c] ++;
+    } else {
+      results[c] = 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters('abba'));
+console.log(countLetters('rodeo'));
+// assertEqual(countLetters('abba'), {a: 2, b: 2});
+// assertEqual(countLetters('rodeo'), {a: 2, b: 2});
