ec15cbe02f9a7861fa0fba912b28164caaf5f5b0
Dan Pletzke <dpletzke@gmail.com>
2020-09-19 20:08:08 +0000

    linting fixes

--- eqArrays.js
+++ eqArrays.js
@@ -9,17 +9,18 @@ const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for(let i = 0; i < arr1.length; i++) {
-    if(Array.isArray(arr1[i])) {
-      if(!eqArrays(arr1[i], arr2[i]))
-      return false;
-    }
-    else if(arr1[i] !== arr2[i]) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+      
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
-} 
+};
 
 
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
