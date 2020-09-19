d539490e48c6bb331ac29c283a415fe923e039b0
Dan Pletzke <dpletzke@gmail.com>
2020-09-19 19:31:34 +0000

    generalize for nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -27,6 +27,10 @@ const eqObjects = function(object1, object2) {
       if (!eqArrays(object1[prop], object2[prop])) {
         return false;
       }
+      /* */
+    } else if (typeof object1[prop] === 'object' && object1[prop] !== null ) {
+      return eqObjects(object1[prop], object2[prop]);
+
     } else if (object1[prop] !== object2[prop]) {
       return false;
     }
@@ -35,17 +39,8 @@ const eqObjects = function(object1, object2) {
 };
 
 
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-const abc = { a: "1", b: "2", c: "3" };
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-const cd2 = { c: "1", d: ["2", 3, 4] };
-
-
-assertEqual(eqObjects(ab, ba), true);
-assertEqual(eqObjects(ab, abc), false);
-assertEqual(eqObjects(cd, dc), true);
-assertEqual(eqObjects(cd, cd2), false);
 
