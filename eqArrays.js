fa336b7d2b5d0dc64450b10f65dd835cb033ba6f
Dan Pletzke <dpletzke@gmail.com>
2020-09-19 20:06:48 +0000

    generalize to allow for infinitely nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -10,7 +10,11 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
   for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
+    if(Array.isArray(arr1[i])) {
+      if(!eqArrays(arr1[i], arr2[i]))
+      return false;
+    }
+    else if(arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -18,4 +22,6 @@ const eqArrays = function(arr1, arr2) {
 } 
 
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
