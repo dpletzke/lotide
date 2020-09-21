10e1345841fbfbf7141282a4cfaf2b6c0071ecbe
Dan Pletzke <dpletzke@gmail.com>
2020-09-21 18:50:30 +0000

    removed console logs from the tests, now they dont do anything, assumed these functions are depreceated

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,19 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const { assert, expect } = require('chai');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe('#middle', () => {
+  it('should return an empty array when passed an array with a single element', () => {
+    const arr = [1];
+    expect(middle(arr)).to.be.an('array').that.is.empty;
+  });
+  it('should return an empty array when passed an array with two elements', () => {
+    const arr = [1, 2];
+    expect(middle(arr)).to.be.an('array').that.is.empty;
+  });
+});
+
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
