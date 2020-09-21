3d533996b08dbaab636dd5b50460ed74e08a5a72
Dan Pletzke <dpletzke@gmail.com>
2020-09-21 18:48:58 +0000

    removed console logs from the tests, now they dont do anything, assumed these functions are depreceated

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,8 +1,8 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2]);
-assertArraysEqual([1, 2, 'apples'], [1, 2, 'apples']);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [1, 2]);
+// assertArraysEqual([1, 2, 'apples'], [1, 2, 'apples']);
 
